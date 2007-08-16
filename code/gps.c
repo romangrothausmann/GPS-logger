67761cd70f7179276073e39da3e9309c48914a16
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2007-08-16 14:11:45 +0200

    commit based on gps_disp_t03.c

--- code/gps.c
+++ code/gps.c
@@ -10,9 +10,9 @@
 #include <avr/pgmspace.h>
 #include "font6x8s.h" //defines prog_char Font [256] [6] 
 
-#define LCD_SI_PIN  PB1
+#define LCD_SI_PIN  PB2
 #define LCD_SI_PORT 'B' //PORTB
-#define LCD_CK_PIN  PB0
+#define LCD_CK_PIN  PB1
 #define LCD_CK_PORT 'B' //PORTB
 #define LCD_A0_PIN  PE4
 #define LCD_A0_PORT 'E' //PORTE
@@ -22,8 +22,22 @@
 #define LCD_CS_PORT 'E' //PORTE 
 
 #define LCD_PIXEL_BYTES 1024
+#ifndef F_CPU
+#define F_CPU 8000000L    // Systemclock,  L not UL!
+#endif
+ 
+#define BAUD 9600L        // Baudrate, L not UL!
+ 
+// Berechnungen
+#define UBRR_VAL ((F_CPU+BAUD*8)/(BAUD*16)-1)   // clever round
+#define BAUD_REAL (F_CPU/(16*(UBRR_VAL+1)))     // Real baudrate
+#define BAUD_ERROR ((BAUD_REAL*1000)/BAUD-1000) // Error in ppm 
 
+#if ((BAUD_ERROR>10) || (BAUD_ERROR<-10))
+  #error Systematic error: baudrate error > 1% and there for too high! Check if F_CPU is without U! 
+#endif
 
+volatile uint8_t uart0_byte, uart0_rec, uart1_byte, uart1_rec;
 
 char setpin(char port, char pin, char state){
     switch(port){
@@ -108,8 +122,11 @@ void lcd_write(uint8_t byte, uint8_t type){
     //setpin(LCD_CS_PORT, LCD_CS_PIN, 0); //select chip, could be moved outside for performance
     setpin(LCD_A0_PORT, LCD_A0_PIN, type); //set A0
     for (i= 7; i >= 0; i--){
+//        _delay_ms(100);
         setpin(LCD_SI_PORT, LCD_SI_PIN, (byte & (1 << i))); //prepare SI
+//        _delay_ms(100);
         setpin(LCD_CK_PORT, LCD_CK_PIN, 1); //validate SI with rising edge
+//        _delay_ms(100);
         setpin(LCD_CK_PORT, LCD_CK_PIN, 0);
         }
 
@@ -242,7 +259,30 @@ char lcd_set_byte(uint8_t byte, uint8_t col, uint8_t page, uint8_t inv, uint8_t 
         }
     return(1);
     }
+/*
+char lcd_iset_byte(uint8_t byte, uint8_t col, uint8_t page, uint8_t inv, uint8_t trans){ 
+    //sets byte instantly on lcd
 
+    uint16_t i;
+
+    if (col > 127 || page > 63)
+        return(0);
+    i= page * 128 + col; 
+    if (inv) {
+        if (trans)
+            m[i]&= ~(byte);
+        else 
+            m[i]= ~(byte);
+        }
+    else {
+        if (trans)
+            m[i]|= byte;
+        else 
+            m[i]= byte;
+        }
+    return(1);
+    }
+*/
 void lcd_write_matrix(uint8_t* m){
     uint16_t i;
     uint8_t page, col;
@@ -303,11 +343,24 @@ char lcd_putchar(uint8_t c, uint8_t col, uint8_t page, uint8_t inv, uint8_t tran
         lcd_set_byte(pgm_read_byte(&Font[c][i]), col + i, page, inv, trans, m); //_far needed???
     return(1);
     }
+/*
+char lcd_iputchar(uint8_t c, uint8_t col, uint8_t page, uint8_t inv, uint8_t trans){
+//puts char instantly on lcd
 
+    uint8_t width, i;
+
+    if (c < 32 )
+        return(0);
+    width= 127 - col < FONT_WIDTH ? 127 - col : FONT_WIDTH; //check if to draw over the border, for running text;)
+    for (i= 0; i < FONT_WIDTH; i++)
+        lcd_set_byte(pgm_read_byte(&Font[c][i]), col + i, page, inv, trans, m); //_far needed???
+    return(1);
+    }
+*/
 char lcd_write_str(char* c, uint8_t col, uint8_t page, uint8_t inv, uint8_t trans, uint8_t* m){//returns displayed chars
 //display a string, needs no write, use' \n'!
 
-    uint8_t i= 0, wrap= 0; //for 6x8 there can be 168 full chars on the display
+    uint8_t i= 0; //for 6x8 there can be 168 full chars on the display
 
     while (c[i]) {
         if (c[i] == '\n'){
@@ -337,69 +390,89 @@ void lcd_drawchar(char c, uint8_t x, uint8_t y, uint8_t trans, uint8_t* m){//a c
 
     }
 
+void uart1_init(uint16_t baud){//51 for 9600bps
+    UBRR1H = (unsigned char)(baud>>8);
+    UBRR1L = (unsigned char)baud;
+    UCSR1C|= (3<<UCSZ10);//async, 8N1, see also UCSZn2!
+    UCSR1B|= (1 << RXCIE1)|(1<<RXEN1); //enable recieve interrupt
+    }
+
+void uart0_init(uint16_t baud){//51 for 9600bps
+//    UBRR0H = (unsigned char)(baud >> 8);
+//    UBRR0L = (unsigned char)baud;
+    UBRR0H = UBRR_VAL >> 8;
+    UBRR0L = UBRR_VAL & 0xFF;
+    UCSR0C|= (3<<UCSZ00);//async, 8N1, see also UCSZn2! (1<<UMSEL0) sync
+    UCSR0B|= (1<<RXEN0)|(1<<TXEN0); //enable recieve interrupt
+    }
+
+
+
 int main(void){
-    uint8_t x, y, j;
-    uint16_t i;
+    //uint8_t x, y, j;
+    //uint16_t i;
     uint8_t  new[LCD_PIXEL_BYTES], old[LCD_PIXEL_BYTES];
     uint8_t *n, *o;
+    char s[2];
 
     n= new;
     o= old;
 
-    DDRB= 0x43;  //0100 0011
-    DDRE= 0x1C;  //0001 1100
+    DDRB= 0x46;  //0100 0110 //lcd on, SI, CK
+    DDRE= 0x1C;  //0001 1100 //A0, RST, CS
+    
+    DDRD= 0x80;  //1000 0000 //gps on
+
+
 
     lcd_init(); // must be executed at the very beginning!
     lcd_clear(n);
 
 
+    //uart0_init(51);
+    //uart1_init(51);
 
+    //while (!(UCSR0A & (1<<UDRE0))) {}
+    //UDR0='H';
+    //setpin('B', PB6, 1);//PORTB|= (1 << PB6); //set backlight on
+    //setpin('D', PD7, 1);//switch on gps
 
-
-    //setpin('B', PB6, 1);//PORTB|= (1 << PB6); //set what to on?
-    //  for(;;)
-    //  lcd_command(0xA5);
-    //lcd_data(0xFF);
-    //lcd_data(0xFF);
-    //lcd_command(0x49);
-
-    lcd_set_pixel(0, 10, 1, n);
-    x= 0;
-    y= 0;
-    j= 10;
-    n= lcd_randomize_matrix(n);
-    
-    lcd_write_str("Hello World!\n Go, go!\n Juppey, this actually works great! Incredible this is, wow!", 0, 1, 0, 0, n);
+    lcd_write_str("Hello World!\nGo, go!\nJuppey, this actually works great! Incredible this is, wow!", 0, 1, 0, 0, n);
     lcd_write_matrix(n);
     _delay_ms(128);
     for(;;){
-        /*
-        for(i= 0; i < LCD_PIXEL_BYTES; i++) {//* 8
-            lcd_set_abs_pixel(i,1,n);
-            if (i < j)
-                lcd_set_abs_pixel(LCD_PIXEL_BYTES * 8 - (j - i),0,n);
-            else
-                lcd_set_abs_pixel(i-j,0,n);
-            o= lcd_fw_matrix(n,o);
-            //lcd_write_matrix(m);
-            //_delay_ms(12);
-        
-            lcd_set_byte(0x0F, i, 0, 1, 1, n);
+/*
+        if(uart1_rec){
+            s[0]=uart1_byte;
+            s[1]= 0;
+            lcd_write_str(s, 0, 0, 0, 0, n);
+            uart1_rec= 0;
             }
-        */
-        //lcd_fw_matrix(n, o);
-        lcd_randomize_matrix(n);
-        lcd_write_matrix(n);
-        lcd_write_str("Hello World!\nGo, go!\nJuppey, this actually works great! Incredible this is, wow!", 0, 1, 0, 0, n);
-        //lcd_write_str("Hello World!", 0, 0, 0, 0, n);
-        //lcd_sel();
-        //lcd_data(0x7F);
-        //lcd_des();
-        _delay_ms(128);
-        _delay_ms(128);
-        _delay_ms(128);
-        _delay_ms(128);
+*/
+/*
+        if(uart0_rec){
+            s[0]=uart0_byte;
+            s[1]= 0;
+            lcd_write_str(s, 0, 0, 0, 0, n);
+            uart0_rec= 0;
+            }
+*/
+//        while (!(UCSR0A & (1<<UDRE0))) {}
+//        UDR0='H';
+//    lcd_write_str("Hello World!\nGo, go!\nJuppey, this actually works great! Incredible this is, wow!", 0, 1, 0, 0, n);
+
         }
     
     return(0);
+    }
+
+/*
+ISR(USART0_RX_vect){
+    uart0_byte= UDR0;
+    uart0_rec= 1;
+    }
+*/
+ISR(USART1_RX_vect){
+    uart1_byte= UDR1;
+    uart1_rec= 1;
     }
